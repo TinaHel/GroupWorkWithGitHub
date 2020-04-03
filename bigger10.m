1fd52064b3cc97bb3fceb51ff9bb71ead9dce8a0
TinaHel <37100644+TinaHel@users.noreply.github.com>
2020-04-03 10:57:01 +0200

    new code
    I made new code and a word document for you to change have fun

---
+++ bigger10.m
@@ -0,0 +1,13 @@
+function [num10] = bigger10(nEingabe)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+if nEingabe == 10
+    num10 = 'Yeah it is ten'
+else
+    num10 = 'No it is not ten'
+end
+
+end
+
