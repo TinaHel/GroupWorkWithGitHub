9d294a1042e6ac1af2d20930c6a15088baec4e53
TinaHel <37100644+TinaHel@users.noreply.github.com>
2020-04-03 11:01:30 +0200

    Update bigger10.m

--- bigger10.m
+++ bigger10.m
@@ -1,12 +1,12 @@
-function [num10] = bigger10(nEingabe)
+function [num20] = bigger20(nEingabe)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
 
-if nEingabe == 10
-    num10 = 'Yeah it is ten'
+if nEingabe == 20
+    num20 = 'Yeah it is twenty'
 else
-    num10 = 'No it is not ten'
+    num20 = 'No it is not twenty'
 end
 
 end
