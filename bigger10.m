466c67ddd9c2f82b67319a830439acc7d5fac93b
BenedictOtt <62551855+BenedictOtt@users.noreply.github.com>
2020-04-03 14:24:59 +0200

    Update bigger10.m
    Habe eine Frage eingefügt und eine Einteilung der Zahl in <=> 10 eingebaut.

--- bigger10.m
+++ bigger10.m
@@ -1,13 +1,18 @@
-function [num10] = bigger10(nEingabe)
+function [num10] = bigger10
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+prompt = 'What is your Number?';
 
+nEingabe = input(prompt);
 
 if nEingabe == 10
-    num10 = 'Yeah it is ten'
+    num10 = 'Yeah it is ten';
 else
-    num10 = 'No it is not ten'
+    if nEingabe > 10
+        num10 = 'bigger then ten';
+    else
+        num10 = 'lower then ten';
+    end
 end
 
 end
-
